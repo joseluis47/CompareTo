aed04c93f18a18b2843fc3282b64ed8621033bae
jose luis <jluiss@bol.com.br>
2020-09-05 14:40:31 -0300

    Entidades e Programa Principal

---
+++ ConsoleApp5/Entities/Produtos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5.Entities
+{
+    class Produtos
+    { 
+        public string Name { get; set; }
+        public double Valor { get; set; }
+
+        public Produtos()
+        {
+        }
+
+        public Produtos(string name, double valor)
+        {
+            Name = name;
+            Valor = valor;
+        }
+
+        public Produtos (string arquivo)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha = @"C:\c#\produtos.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(linha, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+                throw new Exception("Erro");
+            }
+        }
+    }
+}
